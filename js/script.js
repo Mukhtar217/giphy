dce2263b50d0ae2383cd6ba23614c32a134eab6b
Mukhtar Mahdi <mukhtar.mahdi217@gmail.com>
2019-04-16 21:44:52 +0000

    DONE

--- js/script.js
+++ js/script.js
@@ -4,8 +4,50 @@
 /* global $ */
 
 $("#search-button").click(function(){
+    
+    var input = $("input").val();
+    var mainLink = "https://api.giphy.com/v1/gifs/search?q="+input+"&rating=pg&api_key=dc6zaTOxFJmzC";
+     $.ajax({
+        url: mainLink,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            
+            console.log(input);
+            $('.display').empty();
+            var max = 30;
+            var random = Math.floor(Math.random() * Math.floor(max));
+            $('.display').append("<img class='img' src="+response.data[random].images.original.url+">");
+            
+                                    $('.text-center').empty();
+
+        },
+        
+    }); 
   
   
+});
+
+$("#display-button").click(function(){
+    var input = $("input").val();
+    var mainLink = "https://api.giphy.com/v1/gifs/search?q="+input+"&rating=pg&api_key=dc6zaTOxFJmzC";
+     $.ajax({
+        url: mainLink,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            
+            console.log(input);
+            response.data.forEach(function(element) {
+            console.log(element);
+            $('.display').append("<img class='img' src="+element.images.original.url+">");
+            });
+            
+                                    $('.text-center').empty();
+
+        },
+    }); 
   
+                        
 });
 
